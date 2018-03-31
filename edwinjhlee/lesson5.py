9a2164b99fd4982e694f81f16b6cbe88448d558e
WentingRuan <ethel.ruan@gmail.com>
2018-03-31 23:50:16 +0800

    upload lesson 5: 	list comprehension 	generator comprehension

--- edwinjhlee/lesson5.py
+++ edwinjhlee/lesson5.py
@@ -1,238 +1,159 @@
 # list comprehension
- a = [3] * 3
+a = [3] * 3
+a
 
-In [2]: a
-Out[2]: [3, 3, 3]
+a = [ [1,2,3] ] * 3 #把[1,2,3]这个软连接复制了三遍，但都指向一个内存
+a
+a[0] # 对a[0]操作也是对a操作
+a[0].append(4)
+a
+a[1].append(5)
+a
+#----------------------
 
-In [3]: a = [ [1,2,3] ] * 3
+# list generator 
+b = []
+for i in xrange(3): b.append([1, 2, 3])
 
-In [4]: a
-Out[4]: [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+b
+b[0].append(4) #实打实地对b[0]操作
+b
 
-In [5]: a[0]
-Out[5]: [1, 2, 3]
+def f(a = [1,2,3]): a.append(4); print(a)
 
-In [6]: a[0].append(4)
+f()
+f()
+f() #a默认为一个list[1,2,3]，函数结束时自动返回一个a作为全局变量，这个a会在程序再启动时被重新传进去
 
-In [7]: a
-Out[7]: [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+## 类似例子
+def f(a,L=[]):
+    L.append(a)
+    print(L)
 
-In [8]: a[1].append(5)
+f(1)
+f(2)
+f(3)
 
-In [9]: a
-Out[9]: [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+## 等效上面那个例子
+lst = []
+def f(a,L=lst):
+    L.append(a)
+    return(L)
 
-In [10]: b = []
+f(1)
+f(2)
+f(3)
+#默认值是指向一个列表对象，程序开始的时候这个列表对象是空。而不是每次调用函数的时候新建一个空列表。
 
-In [11]: for i in xrange(3): b.append([1, 2, 3])
+# generator comprehension
+## iterate an generator
+c = xrange(0,100)
+c ## c是一个generator，只有要用时才生成
+c = iter(c)
+next(c)
+next(c)
+next(c)
 
-In [12]: b
-Out[12]: [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+## turn a generator into a list
 
-In [13]: b[0].append(4)
+a = [ (1) for i in xrange(3) ]
+a
+b = list((1) for i in xrange(3))
+b ## a == b
 
-In [14]: b
-Out[14]: [[1, 2, 3, 4], [1, 2, 3], [1, 2, 3]]
+d = [ i for i in xrange(3) ]
+d
 
-In [15]: [1,2,3]
-Out[15]: [1, 2, 3]
+d = [ i for i in xrange(10) if i % 2 == 0 ]
+d
 
-In [16]: list(1,2,3)
----------------------------------------------------------------------------
-TypeError                                 Traceback (most recent call last)
-<ipython-input-16-86b0f2b7ea1f> in <module>()
-----> 1 list(1,2,3)
-
-TypeError: list() takes at most 1 argument (3 given)
-
-In [17]: def f(a = [1,2,3]): a.append(4); print(a)
-
-In [18]: f()
-[1, 2, 3, 4]
-
-In [19]: f()
-[1, 2, 3, 4, 4]
-
-In [20]: f()
-[1, 2, 3, 4, 4, 4]
-
-In [21]: a =
-#generator comprehension
-
-In [1]: a = [ (1) for i in xrange(3) ]
-
-In [2]: a
-Out[2]: [1, 1, 1]
-
-In [3]: a = [ i for i in xrange(3) ]
-
-In [4]: a
-Out[4]: [0, 1, 2]
-
-In [5]: a = [ i for i in xrange(10) if i % 2 == 0 ]
-
-In [6]: a = [ i for i in xrange(10) if i % 2 == 0 ]
-
-In [7]: def f(n): s = 1; for i in xrange(1, n+1): s *= i; return s
-  File "<ipython-input-7-ee478d6fde2b>", line 1
-    def f(n): s = 1; for i in xrange(1, n+1): s *= i; return s
-                       ^
-SyntaxError: invalid syntax
+d = [ i for i in xrange(10) if i % 2 == 0 ]
+d
 
 
-In [8]: def f(n): s = 1; for i in xrange(1, n+1): s *= i; return s
-  File "<ipython-input-8-ee478d6fde2b>", line 1
-    def f(n): s = 1; for i in xrange(1, n+1): s *= i; return s
-                       ^
-SyntaxError: invalid syntax
+def f(n): 
+  s = 1
+  for i in xrange(1, n+1):
+     s *= i
+     
+  return(s)
+
+f(1)
+f(2)
+f(3)
+
+# reduce会对参数序列中元素进行累积。
+# reduce(function, iterable[, initializer])
+  # iterable[, initializer] == [1,2,3]
+  #用传给reduce中的函数 function（有两个参数）先对集合中的第 1、2 个元素进行操作，得到的结果再与第三个数据用 function 函数运算，最后得到一个结果。
+# INIT[1,2,3,4,5]
+# reduce(f,INIT[1,2,3,4,5]) == f(f(f(f(f(1,1),2),3),4),5)
+
+reduce(lambda e, a: e*a, range(3), 1) # a *= 0*a
+
+reduce(lambda e, a: e*a, range(1,3),1) # e=1,a=1*1; e=2,a=1*2
+reduce(lambda e, a: e*a, range(1,3),2) # e=1,a=1*2; e=2,a=2*2
+reduce(lambda e, a: e*a, range(1,3),3) # e=1,a=1*3; e=2,a=3*2
 
 
-In [9]: reduce(lambda e, a: e*a, range(10), 1)
-Out[9]: 0
+reduce(lambda e, a: e*a, range(1, 5), 1)
+reduce(lambda e, a: e*a, iter(xrange(1, 5)), 1) #range(1,3)==[1,2]
 
-In [10]: reduce(lambda e, a: e*a, range(1, 11), 1)
-Out[10]: 3628800
-
-In [11]: sum(3, lambda e, a: e*a)
----------------------------------------------------------------------------
-TypeError                                 Traceback (most recent call last)
-<ipython-input-11-e526ce5cd6a3> in <module>()
-----> 1 sum(3, lambda e, a: e*a)
-
-TypeError: 'int' object is not iterable
-
-In [12]: dir(sum)
-Out[12]:
-['__call__',
- '__class__',
- '__cmp__',
- '__delattr__',
- '__doc__',
- '__eq__',
- '__format__',
- '__ge__',
- '__getattribute__',
- '__gt__',
- '__hash__',
- '__init__',
- '__le__',
- '__lt__',
- '__module__',
- '__name__',
- '__ne__',
- '__new__',
- '__reduce__',
- '__reduce_ex__',
- '__repr__',
- '__self__',
- '__setattr__',
- '__sizeof__',
- '__str__',
- '__subclasshook__']
-
-In [13]: reduce(lambda e, a: e*a, range(1, 11), 1)
-Out[13]: 3628800
-
-In [14]: def f(n): return reduce(lambda e, a: e*a, range(1, n+1), 1)
-
-In [15]: a = [ f(i) for i in xrange(0, 10) if i % 2 == 0 ]
-
-In [16]: a
-Out[16]: [1, 2, 24, 720, 40320]
-
-In [17]: map(f, xrange(0, 10))
-Out[17]: [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
-
-In [18]: map(f, xrange(0, 10, 2))
-Out[18]: [1, 2, 24, 720, 40320]
-
-In [19]: map(f, filter(lamda e: e%2==0, xrange(0, 10)))
-  File "<ipython-input-19-f4204bbee1e9>", line 1
-    map(f, filter(lamda e: e%2==0, xrange(0, 10)))
-                        ^
-SyntaxError: invalid syntax
+reduce(lambda e, a: e*a, range(1, 11), 1)
 
 
-In [20]: map(f, filter(lamda e: e%2==0, xrange(0, 10)))
-  File "<ipython-input-20-f4204bbee1e9>", line 1
-    map(f, filter(lamda e: e%2==0, xrange(0, 10)))
-                        ^
-SyntaxError: invalid syntax
+def f(n): return reduce(lambda e, a: e*a, range(1, n+1), 1)
 
+f(1)
+f(2)
+f(4)
+f(6)
+f(8)
 
-In [21]: filter(lambda e: e%2 == 0, xrange(10))
-Out[21]: [0, 2, 4, 6, 8]
+a = [ f(i) for i in xrange(0, 10) if i % 2 == 0 ]
+a #f(0),f(2),f(4),f(6),f(8)
 
-In [22]: map(f, filter(lamda e: e%2==0, xrange(0, 10)))
-  File "<ipython-input-22-f4204bbee1e9>", line 1
-    map(f, filter(lamda e: e%2==0, xrange(0, 10)))
-                        ^
-SyntaxError: invalid syntax
+#map(function,iterable,...) 会根据提供的函数对指定序列做映射。
+  #第一个参数 function 以参数序列中的每一个元素调用 function 函数，返回包含每次 function 函数返回值的新列表。
 
+map(f, xrange(0, 10))
+map(f, xrange(0, 10, 2))
 
-In [23]: map(f, filter(lambda e: e%2==0, xrange(0, 10) ) )
-Out[23]: [1, 2, 24, 720, 40320]
+#filter(function,iterable) 函数用于过滤序列，过滤掉不符合条件的元素，返回由符合条件元素组成的新列表。
+  #filter接收两个参数，第一个为函数，第二个为序列，序列的每个元素作为参数传递给函数进行判，然后返回 True 或 False，最后将返回 True 的元素放到新列表中。
+map(f, filter(lambda e: e%2==0, xrange(0, 10) ) )
 
-In [24]: [ f(i) for i in xrange(0, 10) if i % 2 == 0 ]
-Out[24]: [1, 2, 24, 720, 40320]
+[ f(i) for i in xrange(0, 10) if i % 2 == 0 ]
 
-In [25]: xrange(1000000)
-Out[25]: xrange(1000000)
+### 复习generator
 
-In [26]: iter(xrange(1000000))
-Out[26]: <rangeiterator at 0x63583f0>
+xrange(1000000) #python3里range = xrange
+iter(xrange(1000000)) ￥rangeiterator object
 
-In [27]: iter(xrange(1000000))
-Out[27]: <rangeiterator at 0x63587b0>
+a = iter(xrange(1000000))
+next(a)
+next(a)
+next(a)
 
-In [28]: a = iter(xrange(1000000))
-
-In [29]: next(a)
-Out[29]: 0
-
-In [30]: next(a)
-Out[30]: 1
-
-In [31]: next(a)
-Out[31]: 2
-
-In [32]: a = iter(range(1000))
-
-In [33]: next(a)
-Out[33]: 0
-
-In [34]: next(a)
-Out[34]: 1
-
-In [35]:
+a = iter(range(1000))
+next(a)
+next(a)
 
 #lazy reaction
 
->>> f = lambda n: reduce(lambda e, a: e* a, xrange(1, n+1), 1)
->>> f(3)
-6
->>> t = ( f(i) for i in xrange(10000) )
->>> t[0]
+f = lambda n: reduce(lambda e, a: e* a, xrange(1, n+1), 1)
+f(3)
 
-Traceback (most recent call last):
-  File "<pyshell#3>", line 1, in <module>
-    t[0]
-TypeError: 'generator' object has no attribute '__getitem__'
->>> a = iter(a)
+t = ( f(i) for i in xrange(10000) ) #t是一个tupple generator
+t[0] #
 
-Traceback (most recent call last):
-  File "<pyshell#4>", line 1, in <module>
-    a = iter(a)
-NameError: name 'a' is not defined
->>> a = iter(t)
->>> next(a)
-1
->>> next(a)
-1
->>> next(a)
-2
->>> next(a)
-6
->>> next(a)
-24
->>> t = ( f(i) for i in xrange(10000) )
+a = iter(t)
+next(a)
+next(a)
+
+t = ( f(i) for i in xrange(10000) )
+t = iter(t)
+next(t)
+next(t)
+
+
